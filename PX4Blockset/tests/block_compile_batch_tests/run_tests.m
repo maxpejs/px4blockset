75df2edafbd6df189bdd84fca404fd18e8ff906c
Max Pejs <max.pejs@it-designers.de>
2018-10-19 17:07:30 +0200

    compile tests added

---
+++ PX4Blockset/tests/block_compile_batch_tests/run_tests.m
@@ -0,0 +1,28 @@
+clc;
+c = tic;
+
+fileList = dir;
+
+for i = 1 : length(fileList)
+    if ~fileList(i).isdir
+        fileName = fileList(i).name;
+        
+        [pathstr, name, ext] = fileparts(fileName);
+        
+        if strcmpi(ext, '.mdl')
+            rtwbuild(name)
+            bdclose all
+            try 
+                rmdir([name '_px4blockset'], 's');
+            catch
+            end
+        end
+    end
+end
+
+
+rmdir('slprj', 's');
+c = toc(c);
+disp(['Block (gen+compile) tests took: ' num2str(c) ' s']);
+
+msgbox('Tests OK', '', 'none');
