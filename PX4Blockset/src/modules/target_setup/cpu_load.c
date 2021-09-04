dbf313ad6848efbd9fe9f0839ab6842eb215e653
Max Pejs <max.pejs@googlemail.com>
2021-09-05 00:16:31 +0200

    renamed

--- PX4Blockset/src/modules/target_setup/cpu_load.c
+++ PX4Blockset/src/modules/target_setup/cpu_load.c
@@ -1,44 +1,45 @@
 #include "cpu_load.h"
 #include "string.h"
 
-static uint32_t _cpu_load_max = 0;
-static uint32_t _cpu_load_act = 0;
+static uint32_t cpu_load_max = 0;
+static uint32_t cpu_load_act = 0;
 
 void cpu_load_update()
 {
-	static uint32_t last_summ_RunTimeCounter = 0;
-	static uint32_t last_total = 0;
-
+	static uint32_t last_cpu_load = 0;
+	static uint32_t last_os_runtime_total = 0;
 
 	TaskStatus_t val[10];
-	uint32_t total = 1;
-	UBaseType_t size = uxTaskGetSystemState((TaskStatus_t * const ) &val, 10, &total);
+	uint32_t os_runtime_total = 1;
+	UBaseType_t size = uxTaskGetSystemState((TaskStatus_t * const ) &val, 10, &os_runtime_total);
 
-	uint32_t cpuload = 0;
+	uint32_t cpu_load = 0;
 
 	for (uint32_t i = 0; i < size; i++)
 	{
 		if(strcmp(val[i].pcTaskName, "IDLE") == 0)
+		{
 			continue; // skip
+		}
 
-		cpuload += val[i].ulRunTimeCounter;
+		cpu_load += val[i].ulRunTimeCounter;
 	}
 
-	_cpu_load_act = ((uint64_t) (cpuload - last_summ_RunTimeCounter) * 100) / (total - last_total);
+	cpu_load_act = ((cpu_load - last_cpu_load) * 100) / (os_runtime_total - last_os_runtime_total);
 
-	if (_cpu_load_act > _cpu_load_max)
-		_cpu_load_max = _cpu_load_act;
+	if (cpu_load_act > cpu_load_max)
+		cpu_load_max = cpu_load_act;
 
-	last_total = total;
-	last_summ_RunTimeCounter = cpuload;
+	last_os_runtime_total = os_runtime_total;
+	last_cpu_load = cpu_load;
 }
 
 uint32_t cpu_load_get_curr_cpu_load(void)
 {
-	return _cpu_load_act;
+	return cpu_load_act;
 }
 
 uint32_t cpu_load_get_max_cpu_load(void)
 {
-	return _cpu_load_max;
+	return cpu_load_max;
 }
