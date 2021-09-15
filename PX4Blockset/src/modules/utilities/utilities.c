5ed50ad6755f3533ca697f8a7fb866407a848e12
Max Pejs <max.pejs@googlemail.com>
2021-09-15 17:04:24 +0200

    add max func

--- PX4Blockset/src/modules/utilities/utilities.c
+++ PX4Blockset/src/modules/utilities/utilities.c
@@ -21,3 +21,8 @@ size_t min(size_t a, size_t b)
 {
 	return a < b ? a : b;
 }
+
+size_t max(size_t a, size_t b)
+{
+	return a > b ? a : b;
+}
--- PX4Blockset/src/modules/utilities/utilities.h
+++ PX4Blockset/src/modules/utilities/utilities.h
@@ -4,8 +4,11 @@
 #include <stddef.h>
 
 uint8_t ascii_2_nibble(uint8_t c);
+
 void uppercase(char * buff);
 
 size_t min(size_t a, size_t b);
+
+size_t max(size_t a, size_t b);
 
 #endif // ASCII_CONVERT_H
