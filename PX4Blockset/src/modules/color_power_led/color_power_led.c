c08cf3180ce9f9496560657ba8b481c102abdc09
Max Pejs <max.pejs@googlemail.com>
2021-09-15 19:02:52 +0200

    renamed var

--- PX4Blockset/src/modules/color_power_led/color_power_led.c
+++ PX4Blockset/src/modules/color_power_led/color_power_led.c
@@ -1,7 +1,7 @@
 #include "color_power_led.h"
 
 uint32_t module_state = DISABLE;
-color_led_data_st rgb_color[2];
+color_led_data_st rgb[2];
 uint32_t storage_idx;
 uint32_t updated;
 
@@ -18,7 +18,7 @@ void px4_color_power_led_init()
 	px4_i2c_drv_init(COLOR_LED_I2C_ITF);
 	px4_i2c_drv_set_clock_speed(COLOR_LED_I2C_ITF, COLOR_LED_CLK_SPEED);
 
-	memset(&rgb_color, 0, sizeof(rgb_color));
+	memset(&rgb, 0, sizeof(rgb));
 	storage_idx = 0;
 	updated = 0;
 
@@ -41,7 +41,7 @@ void px4_color_power_led_update()
 		return;
 	}
 
-	if (rgb_color[0].r == rgb_color[1].r && rgb_color[0].g == rgb_color[1].g && rgb_color[0].b == rgb_color[1].b)
+	if (rgb[0].r == rgb[1].r && rgb[0].g == rgb[1].g && rgb[0].b == rgb[1].b)
 	{
 		// nothing changed since last call, so nothing to do
 		return;
@@ -53,9 +53,9 @@ void px4_color_power_led_update()
 	uint32_t idx = storage_idx;
 	storage_idx = (storage_idx + 1) % 2;
 
-	i2c_msg[1] = (uint8_t) rgb_color[idx].b;
-	i2c_msg[3] = (uint8_t) rgb_color[idx].g;
-	i2c_msg[5] = (uint8_t) rgb_color[idx].r;
+	i2c_msg[1] = (uint8_t) rgb[idx].b;
+	i2c_msg[3] = (uint8_t) rgb[idx].g;
+	i2c_msg[5] = (uint8_t) rgb[idx].r;
 
 	int result = px4_i2c_drv_transmit(COLOR_LED_I2C_ITF, COLOR_LED_I2C_DEV_ADDR, i2c_msg, sizeof(i2c_msg));
 
@@ -69,9 +69,9 @@ void px4_color_power_led_update()
 void px4_color_power_led_set(uint32_t r, uint32_t g, uint32_t b)
 {
 	// rescale from 0...255 to 0...15, divide by 16 = 2^4 => shift 4 bits to right
-	rgb_color[storage_idx].r = (r >> 4);
-	rgb_color[storage_idx].g = (g >> 4);
-	rgb_color[storage_idx].b = (b >> 4);
+	rgb[storage_idx].r = (r >> 4);
+	rgb[storage_idx].g = (g >> 4);
+	rgb[storage_idx].b = (b >> 4);
 	updated = 1;
 }
 
